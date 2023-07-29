1370c1a89c93ea38ff7a75120eb8a7222e1bbcab
codescaptain <ahmet-57-@hotmail.com>
2023-07-29 17:13:32 +0300

    create home screen and add tailwind class

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        customColor: '#494a4f',
+      },
     },
   },
   plugins: [
