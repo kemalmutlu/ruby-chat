fa2adf957d02e7c07a903b6abba2f24caab78949
deniziren <deniz5734@hotmail.com>
2023-07-30 16:07:56 +0300

    add-ask-service-to-controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,14 +9,11 @@ class ResultsController < ApplicationController
   end
 
   def create
-    @result = Result.new(result_params)
-    @result.answer = Faker::Quote.famous_last_words #Later will use main service
+    @result = Ask.new(result_params[:question]).fetch_result
 
     respond_to do |format|
-      if @result.save!
-        format.turbo_stream do
-          render(turbo_stream: turbo_stream.append('results', partial: 'results/result', locals: { result: @result }))
-        end
+      format.turbo_stream do
+        render(turbo_stream: turbo_stream.append('results', partial: 'results/result', locals: { result: @result }))
       end
     end
   end
