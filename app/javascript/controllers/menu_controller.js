b0976aa734ce72eab1b42f99cb641fa742db9b26
codescaptain <ahmet-57-@hotmail.com>
2023-07-30 22:22:57 +0300

    fix open/close menu bar

---
+++ app/javascript/controllers/menu_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["openMenu", "closeMenu", "leftMenu"];
+
+    toggleMenu(){
+        let left_menu = this.leftMenuTarget
+
+        if (left_menu.dataset.hidden === "true") {
+            left_menu.style.display = "none";
+            left_menu.dataset.hidden = "false"
+            this.openMenuTarget.classList.remove("hidden");
+        } else {
+            // we did not give block because we use tailwind and the element has a display style
+            left_menu.style.display = "";
+            left_menu.dataset.hidden = "true"
+            this.openMenuTarget.classList.add("hidden");
+        }
+    }
+}
