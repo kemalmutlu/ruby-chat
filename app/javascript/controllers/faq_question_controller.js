27a0792f4f1b0261ff3363efcaad7fe2f67cf738
Atakan Acar <atakanacar19999@hotmail.com>
2023-07-31 00:45:28 +0300

    fix faq question controller data

--- app/javascript/controllers/faq_question_controller.js
+++ app/javascript/controllers/faq_question_controller.js
@@ -19,14 +19,14 @@ export default class extends Controller {
                     .then(response => response.json())
                     .then(data => {
                         const newContent = `
-                          <div id="${data.id}" class="my-[20px] min-w-full border border-gray-300 text-gray-900 bg-gray-600 rounded-lg">
-                            <div class="flex flex-row justify-end text-right">
-                              <span class="max-w-[75%] px-2 py-2">
-                                ${data.question}
+                          <div id="${data.id}" class="my-[20px] min-w-full">
+                            <div class="flex flex-row justify-end text-left">
+                              <span class="max-w-[75%] px-2 py-2 my-[20px] border border-gray-300 text-gray-900 bg-gray-600 rounded-lg">
+                                ${data.question.replace("You are a Ruby on Rails developer, ", "")}
                               </span>
                             </div>
                             <div class="flex flex-row justify-start text-left">
-                              <span class="max-w-[75%] px-2 py-2">
+                              <span class="max-w-[75%] px-2 py-2 my-[20px] border border-gray-300 text-gray-900 bg-gray-600 rounded-lg">
                                 ${data.answer}
                               </span>
                             </div>
