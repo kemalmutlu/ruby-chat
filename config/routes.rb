9b3e1a586dfb6043ad3a4c975394cbaf428c4027
deniziren <deniz5734@hotmail.com>
2023-07-30 23:49:42 +0300

    remove previous results after page re-rendered

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "results#index"
   resources :results do
     get :append_result, on: :member
   end
