96c2b1ae20a14d151d20a57e0950c36c87b0c9c6
Atakan Acar <atakanacar19999@hotmail.com>
2023-07-30 16:18:14 +0300

    add db extensions

--- db/migrate/20230729115330_create_results.rb
+++ db/migrate/20230729115330_create_results.rb
@@ -1,5 +1,9 @@
 class CreateResults < ActiveRecord::Migration[7.0]
   def change
+    enable_extension('pg_trgm') unless extensions.include?('pg_trgm')
+    enable_extension('pgcrypto') unless extensions.include?('pgcrypto')
+    enable_extension('uuid-ossp') unless extensions.include?('uuid-ossp')
+
     create_table :results, id: :uuid do |t|
       t.text :question, null: false
       t.text :answer, null: false
