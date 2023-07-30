6c5abc77b6f938deb579002ba86adc2eab8c909f
Atakan Acar <atakanacar19999@hotmail.com>
2023-07-30 13:25:24 +0300

    results factory annotate

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id         :uuid             not null, primary key
+#  answer     :text             not null
+#  count      :integer          default(1), not null
+#  question   :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_results_on_question_and_answer  (question,answer)
+#
 FactoryBot.define do
   factory :result do
     question { Faker::Quote.famous_last_words }
