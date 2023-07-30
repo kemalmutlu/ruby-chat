0d4f5b42865adb562dea305f0b09c7c659300813
Atakan Acar <atakanacar19999@hotmail.com>
2023-07-30 13:12:56 +0300

    feature: update algolia service with increment and reindex

--- app/services/algolia_search/request.rb
+++ app/services/algolia_search/request.rb
@@ -1,13 +1,18 @@
 module AlgoliaSearch
   class Request
-
     def initialize(message)
-      raise "Message parameter is empty" if message.empty?
+      raise 'Message is empty' if message.empty?
+
       @message = message
     end
-    
-    def get_data
-      Result.algolia_search(@message&.slice(0, 512))
+
+    def fetch_data
+      results_ids = Result.raw_search(@message&.slice(0, 512)).dig('hits')&.pluck('id') || []
+      results = Result.where(id: results_ids)
+
+      results.increment_all
+      results.reindex!
+      results
     end
   end
 end
