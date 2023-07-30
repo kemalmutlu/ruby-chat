4f7e911e1e58002d6bda03745d0a7309fad4ec8e
Atakan Acar <atakanacar19999@hotmail.com>
2023-07-30 23:44:42 +0300

    add loading icon

--- app/javascript/controllers/reset_question_controller.js
+++ app/javascript/controllers/reset_question_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["loading", "textField", "submitButton"];
+
+  start_submit() {
+    this.textFieldTarget.disabled = true;
+    this.loadingTarget.hidden = false;
+  }
+
   reset() {
-    this.element.reset()
+    this.element.reset();
+
+    this.textFieldTarget.disabled = false;
+    this.loadingTarget.hidden = true;
   }
 }
